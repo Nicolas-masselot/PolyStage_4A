7254ada8f5804f122721546c626bb7170567254c
Yoann-Adc <YoannAudic@laposte.net>
2021-03-31 14:52:07 +0200

    ajout du login (frontend)

--- PolystagesAngular/frontend/src/app/login/login.component.ts
+++ PolystagesAngular/frontend/src/app/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../auth/auth.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit
+{
 
-  constructor() { }
+  identifiant = "";
+  password = "";
 
-  ngOnInit(): void {
+  constructor(private service: AuthService,
+              private router: Router,
+              private toastr: ToastrService) {}
+
+  ngOnInit(): void
+  {}
+
+  tryConnection()
+  {
+    this.service.sendAuthentication(this.identifiant, this.password).subscribe(r =>
+    {
+      if (r["status"] == "ok")
+      {
+        this.router.navigateByUrl("/eleve");
+      }
+      else
+      {
+        this.toastr.error("Mauvais identifiant / mot de passe");
+      }
+    });
   }
 
 }
