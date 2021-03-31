2f0d202bf0a9aef5ac01d351907048b0195caf3b
Yoann-Adc <YoannAudic@laposte.net>
2021-03-31 13:39:58 +0200

    merge quick fixes

--- PolystagesAngular/frontend/src/app/app.module.ts
+++ PolystagesAngular/frontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    EnteteComponent
+    EnteteComponent,
     MenuComponent,
     LoginComponent
   ],
