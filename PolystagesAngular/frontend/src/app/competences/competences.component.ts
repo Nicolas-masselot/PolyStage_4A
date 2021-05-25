66f3a59ecda90fb6d1669754a02fb765c5682b33
nicolas-masselot <nicolasmasselot0@gmail.com>
2021-05-25 23:18:46 +0200

    interface des formulaires terminée

--- PolystagesAngular/frontend/src/app/competences/competences.component.ts
+++ PolystagesAngular/frontend/src/app/competences/competences.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {MessageService} from "../message/message.service";
+import {AuthService} from "../auth/auth.service";
+import {FormBuilder, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-competences',
@@ -10,19 +12,69 @@ import {MessageService} from "../message/message.service";
 export class CompetencesComponent implements OnInit {
 
   idStage : number | undefined;
+  stage: any ;
 
   competences: any ;
 
-  constructor( private route: ActivatedRoute, private message: MessageService) { }
+  FormComp = this.fb.group({});
+
+  fields = {
+    nomentreprise: undefined,
+    nomtuteur: undefined,
+    annee: undefined,
+    niveau: undefined,
+    nom: undefined,
+    prenom: undefined,
+    idtuteur: undefined,
+    idens: undefined
+
+  }
+
+  constructor( private route: ActivatedRoute, private message: MessageService, private auth : AuthService, private fb : FormBuilder) { }
 
   ngOnInit(): void {
     this.idStage = Number(this.route.snapshot.paramMap.get('stageId')) ;
-    console.log(this.idStage) ;
+    this.message.sendGetMessagebyID('stages/eval/',this.idStage).subscribe( reponse => {
+      // @ts-ignore
+      this.stage = reponse[0] ;
+    }) ;
 
     this.message.sendGetMessageQuery('recupCompetences',null).subscribe( reponse => {
       this.competences =reponse ;
-      console.log(this.competences) ;
+
+      this.fields.nomentreprise = this.stage.nomcomplet ;
+      // @ts-ignore
+      this.fields.nomtuteur = this.auth.prenom + ' ' + this.auth.nom ;
+      this.fields.annee = this.stage.annee;
+      this.fields.niveau = this.stage.niveau;
+      this.fields.nom = this.stage.nom;
+      this.fields.prenom = this.stage.prenom;
+      this.fields.idtuteur = this.stage.idtuteur;
+      this.fields.idens = this.stage.idens;
+
+      for (let categorie of this.competences) {
+        for (let quest of categorie.questions){
+          this.FormComp.addControl(quest.idcompetence,new FormControl()) ;
+        }
+      }
+
     }) ;
+
+
+
+  }
+
+  submitCompetences() : void {
+    Object.keys(this.FormComp.controls).forEach(key => {
+
+      if (this.FormComp.controls[key].value !== null){
+        // @ts-ignore
+        this.fields[key] = this.FormComp.controls[key].value ;
+      }
+
+    });
+    console.log(this.fields) ;
+    //TODO envoyer en post fields
   }
 
 }
--- PolystagesAngular/frontend/src/app/eval/eval.component.ts
+++ PolystagesAngular/frontend/src/app/eval/eval.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {MessageService} from "../message/message.service";
+import {FormBuilder, FormControl} from "@angular/forms";
+import {AuthService} from "../auth/auth.service";
 
 @Component({
   selector: 'app-eval',
@@ -10,23 +12,93 @@ import {MessageService} from "../message/message.service";
 export class EvalComponent implements OnInit {
 
   idStage : number | undefined;
+  stage : any | undefined ;
 
   choixOuiNon: string[] = ["Oui", "Non"] ;
   choixContrat: string[] = ["CDD", "CDI", "Prolongation de stage", "Contrat de thèse en entreprise", "VIE"] ;
   choixClassement: string[] = ["Parmi les meilleurs", "Au dessus de la moyenne", "À la moyenne", "En dessous de la moyenne", "Parmi les plus mauvais"] ;
   choixNotation: string[] = ["Très bien", "Bien", "Moyen", "Insuffisant", "Sans objet"] ;
 
+
+  FormEval = this.fb.group({});
+
+  fields = {
+    annee: undefined,
+    niveau: undefined,
+    nom: undefined,
+    prenom: undefined,
+    idtuteur: undefined,
+    idens: undefined
+  } ;
+
   questions : any ;
-  constructor(private route: ActivatedRoute, private message : MessageService) { }
+  constructor(private route: ActivatedRoute, private message : MessageService, private auth: AuthService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.idStage = Number(this.route.snapshot.paramMap.get('stageId')) ;
-    console.log(this.idStage) ;
+    this.message.sendGetMessagebyID('stages/eval/',this.idStage).subscribe( reponse => {
+      // @ts-ignore
+      this.stage = reponse[0] ;
+    }) ;
 
     this.message.sendGetMessageQuery('recupQuestions',null).subscribe( reponse => {
       this.questions =reponse ;
-      console.log(this.questions) ;
+
+      // @ts-ignore
+      this.fields[2] = this.stage.nom ;
+      // @ts-ignore
+      this.fields[3] = this.stage.prenom ;
+      // @ts-ignore
+      this.fields[4] = this.stage.nomcomplet ;
+
+      // @ts-ignore
+      this.fields[6] = this.auth.prenom+' '+this.auth.nom ;
+
+      this.fields.annee = this.stage.annee ;
+      this.fields.niveau = this.stage.niveau ;
+      this.fields.nom = this.stage.nom ;
+      this.fields.prenom = this.stage.prenom ;
+      this.fields.idtuteur = this.stage.idtuteur ;
+      this.fields.idens = this.stage.idens ;
+
+
+      for (let categorie of this.questions) {
+        for (let quest of categorie.questions){
+            this.FormEval.addControl(quest.idquest,new FormControl()) ;
+        }
+      }
+
+      this.FormEval.get('2')?.setValue(this.stage.nom) ;
+      this.FormEval.get('3')?.setValue(this.stage.prenom) ;
+      this.FormEval.get('4')?.setValue(this.stage.nomcomplet) ;
+      this.FormEval.get('6')?.setValue(this.auth.prenom+' '+this.auth.nom) ;
+
+
     }) ;
+
   }
+
+  QuestionBonNiveau(niveau: any): boolean {
+    if (niveau){
+      return this.stage.niveau >= niveau ;
+    }else {
+      return true ;
+    }
+  }
+
+  submitQuestionnaire() : void {
+
+    Object.keys(this.FormEval.controls).forEach(key => {
+
+      if (this.FormEval.controls[key].value !== null){
+        // @ts-ignore
+        this.fields[key] = this.FormEval.controls[key].value ;
+      }
+
+    });
+    console.log(this.fields) ;
+    //TODO envoyer en post fields
+  }
+
 
 }
